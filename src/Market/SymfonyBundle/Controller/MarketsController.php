41b92ad86ffa480e42f6a9551d0ded427b1aadb1
Albert Ducin <albert.ducin@gmail.com>
2015-10-13 20:43:12 +0200

    added and modified files

--- src/Market/SymfonyBundle/Controller/MarketsController.php
+++ src/Market/SymfonyBundle/Controller/MarketsController.php
@@ -2,16 +2,28 @@
 
 namespace Market\SymfonyBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class MarketsController extends Controller
 {
-    public function indexAction($path)
-    {
-    	$twigPath='MarketSymfonyBundle:Markets:'.$path.'.html.twig';
-        return $this->render($twigPath, array(
-            'path' => $path,
-        ));
-    }
-
+	public function getBookmarks(){
+		$url=array('homepage', 'services', 'rates' , 'about', 'blog', 'contact');
+		foreach($url as $page) {
+			$allURLsGen[] = array('name'=>$page, 'path'=>$this->generateUrl('markets_index', array('path' => $page)));
+    		}
+		return $allURLsGen;
+	}
+	/**
+ 	* @Route("/", defaults={"path" = "homepage"})
+ 	* @Route("/{path}")
+ 	*/
+	public function indexAction($path)
+	{
+		return $this->render('MarketSymfonyBundle:Markets:'.$path.'.html.twig', array(
+			'path' => $path, 'url'=>$this->getBookmarks()
+			));
+	}
 }
