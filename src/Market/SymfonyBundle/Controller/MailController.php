0f3960be5d73fec3b6b1abbb8fa252497b42ef6c
Albert Ducin <albert.ducin@gmail.com>
2015-10-31 13:02:50 +0100

    routing and mailController updated

--- src/Market/SymfonyBundle/Controller/MailController.php
+++ src/Market/SymfonyBundle/Controller/MailController.php
@@ -12,11 +12,12 @@ class MailController extends Controller
 	public function mailSend($mail, $message, $name)
 	{
 		$to      = 'ad9bis@gmail.com';
-		$subject = 'Wiadomość wysłana przez formularz - Terminy giełd';
-		$message = $message;
-		$headers = 'From: '.$name . "\r\n" .
-    		'Reply-To: '.$name . "\r\n" .
-    		'X-Mailer: PHP/' . phpversion();
+		$subject = $name .'- Wiadomość wysłana przez formularz - Terminy giełd';
+		$message = htmlspecialchars($mess, ENT_COMPAT, 'UTF-8');;
+		$headers = "Reply-to: ".$name."<ad9bis@gmail.pl>".PHP_EOL;
+   		$headers .= "From: ".$mail.PHP_EOL;
+  		$headers .= "MIME-Version: 1.0".PHP_EOL;
+  		$headers .= "Content-type: text/html; charset=utf-8".PHP_EOL; 
 		if(mail($to, $subject, $message, $headers))
 		{
 			$result='Email został poprawnie wysłany!';
